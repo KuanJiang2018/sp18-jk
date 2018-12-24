a7f128187f6e2db409d761a2be67482162f16565
Jiang Kuan <jiangkuan2018@163.com>
2018-12-25 06:06:31 +0800

    finish HelloNumbers.java work

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,7 +1,7 @@
 public class HelloNumbers {
     public static void main(String[] args) {
         int x = 0;
-        int sum = 0
+        int sum = 0;
         while (x < 10) {
         	sum += x;
             System.out.print(sum + " ");  
