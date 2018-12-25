e4c5420e238376c35c822541a082ab0c38af20d7
Jiang Kuan <jiangkuan2018@163.com>
2018-12-25 09:13:00 +0800

    IntList.java

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,7 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null) return B;
         IntList iter = A;
         while (iter != null && iter.rest != null) iter = iter.rest;
         iter.rest = B;
